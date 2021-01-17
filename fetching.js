c7a2cf32339b9323607df6a748425ecb681eacf5
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-01-17 14:32:13 +0545

    Add files via upload

---
+++ fetching.js
@@ -0,0 +1,18 @@
+const cards=document.getElementById("card");
+async function test(){
+    const url="https://api.github.com/users/nirajandata/repos";
+    const response =await fetch(url);
+    const result= await response.json();
+    result.forEach(i=>{
+       const anchor=document.createElement("a");
+       const parag=document.createElement("p");
+       anchor.href=i.html_url;
+       anchor.textContent=i.name;
+       parag.textContent=i.description
+       cards.appendChild(anchor);
+       cards.appendChild(document.createElement("br"));
+       cards.appendChild(parag)
+       cards.appendChild(document.createElement("br"));
+    });
+}   
+test();
